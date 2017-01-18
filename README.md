f67e4dbd6110ff27a7bbd9971367636fe812a5b1
Mike Golubitsky <mgolubitsky@solarcity.com>
2017-01-17 21:05:55 -0800

    remove stuff and update readme with live link

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-[Live](https://quiet-garden-14457.herokuapp.com/)  
-
+[Live](https://mysterious-dusk-51947.herokuapp.com/)  
+It took me literally 2 minutes to create this repo and deploy to the above link using [this](https://blog.heroku.com/deploying-react-with-zero-configuration).
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,26 +1,3 @@
-export const takeNextStep = () => ({
-  type: 'STEP'
-})
-
-export const initializeRandom = (percentage) => ({
-  type: 'INITIALIZE_RANDOM',
-  percentage
-})
-
-export const toggleToroidal = () => ({
-  type: 'TOGGLE_TOROIDAL'
-})
-
-export const toggleCell = (cellIndex) => ({
-  type: 'TOGGLE_CELL',
-  cellIndex
-})
-
-export const startGame = (interval) => ({
-  type: 'START_GAME',
-  interval
-})
-
-export const pauseGame = () => ({
-  type: 'PAUSE_GAME'
+export const defaultAction = () => ({
+  type: 'DEFAULT_ACTION'
 })
